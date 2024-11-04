0d0e48dc6c1cd42d4d4d4d35d9a040f793424564
Federico Dionisi <code@fdionisi.me>
2024-11-04 14:47:28 +0100

    chore: workaround the old version of the model

--- crates/anthropic/src/anthropic.rs
+++ crates/anthropic/src/anthropic.rs
@@ -38,9 +38,9 @@ impl FromStr for Model {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "claude-3-5-sonnet-latest" | "claude-3-5-sonnet-20240620" => {
-                Ok(Model::ClaudeThreeDotFiveSonnet)
-            }
+            "claude-3-5-sonnet-latest"
+            | "claude-3-5-sonnet-20241022"
+            | "claude-3-5-sonnet-20240620" => Ok(Model::ClaudeThreeDotFiveSonnet),
             "claude-3-sonnet-20240229" => Ok(Model::ClaudeThreeSonnet),
             "claude-3-opus-20240229" => Ok(Model::ClaudeThreeOpus),
             "claude-3-haiku-20240307" => Ok(Model::ClaudeThreeHaiku),
